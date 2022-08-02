ccb509145faaa4db5fbe9bcafc43e9a1c222cb58
Inaniok <sergiyizotov@gmail.com>
2022-08-02 11:23:23 +0300

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -12,17 +12,19 @@
 //     mobileMenuRef.classList.toggle("is-open");
 //   });
 // })();
-(() => {
-    const refs = {
-      openMenuBtn: document.querySelector("[data-menu-open]"),
-      closeMenuBtn: document.querySelector("[data-menu-close]"),
-      menu: document.querySelector("[data-menu]"),
-    };
-  
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
-  
-    function toggleMenu() {
-      refs.menu.classList.toggle("is-open");
-    }
-  })();
+;(() => {
+  const refs = {
+      openMenuBtn: document.querySelector('.mob-menu-open'),
+      closeMenuBtn: document.querySelector('.mob-menu-close'),
+      menu: document.querySelector('.mob-menu'),
+      body: document.querySelector('body'),
+  }
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu)
+  refs.closeMenuBtn.addEventListener('click', toggleMenu)
+
+  function toggleMenu() {
+      refs.menu.classList.toggle('is-hidden')
+      refs.body.classList.toggle('no-scroll')
+  }
+})()
